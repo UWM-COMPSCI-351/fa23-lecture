9def1adc209cf243f4c90844be114b9358ec61d0
John Boyland <boyland@uwm.edu>
2023-10-18 20:17:43 -0500

    Test for Queue and stubbed class

---
+++ src/edu/uwm/cs351/Queue.java
@@ -0,0 +1,52 @@
+package edu.uwm.cs351;
+
+public class Queue<E> {
+	// a helper method which we will find useful.
+        @SuppressWarnings("unchecked")
+        private E[] makeArray(int size) {
+		return (E[])new Object[size]; // lying...
+        }
+
+	// TODO: data structure
+
+	/** Create an empty queue */
+	public Queue() {
+		// TODO
+	}
+
+	private void ensureCapacity(int s) {
+		// TODO
+	}
+
+	/** Return numerb of elements in queue */
+	public int size() {
+		return -1; // TODO
+	}
+
+	/** Return whether queue is empty */
+	public boolean isEmpty() {
+		return false; // TODO
+	}
+
+	/** Add element to rear of queue.
+	 * @param elem element to add, may be null.
+	 */
+	public void enqueue(E elem) {
+		// TODO
+	}
+
+	/** Return front element of queue after removing it.
+         * @return front element.
+         */
+	public E dequeue() {
+		return null; // TODO
+	}
+
+	/** Return front element of queue without removing it.
+         * @return froint element
+	 */
+	public E front() {
+		return null; // TODO
+	}
+}
+
